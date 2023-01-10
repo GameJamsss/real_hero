4334013aa4ca5254d4cd0d8410994a511ed6e98b
S0u1 <shestaevd26@gmail.com>
2023-01-10 21:29:41 +0400

    fsm

---
+++ Assets/Scripts/StateMachine/AbstractState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.StateMachine
+{
+    
+    public abstract class AbstractState<T>
+    {
+        public HashSet<AbstractState<T>> BlackSet = new HashSet<AbstractState<T>>();
+        public HashSet<AbstractState<T>> WhiteSet = new HashSet<AbstractState<T>>();
+
+        private ulong _priority = 0;
+        public ulong priority
+        {
+            get => _priority;
+            set
+            {
+                _priority = value + 1;
+            }
+        }
+        public string Name;
+        abstract public void OnEnter(T entity);
+        abstract public void InState(T entity);
+        abstract public void OnExit(T entity);
+        virtual public bool EnterCondition(T entity)
+        {
+            return true;
+        }
+        public AbstractState<T> ToBlack(AbstractState<T> state)
+        {
+            BlackSet.Add(state);
+            return this;
+        }
+        public AbstractState<T> ToWhite(AbstractState<T> state)
+        {
+            WhiteSet.Add(state);
+            return this;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,41 @@
+using Assets.Scripts.StateMachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine<T>
+{
+    private AbstractState<T> currentState = State<T>.GetEmpty();
+    private StateManager<T> Manager;
+    private T Entity;
+    public StateMachine(StateManager<T> manager)
+    {
+        Manager = manager;
+        Entity = manager.Entity;
+    }
+    public StateMachine(T entity)
+    {
+        Entity = entity;
+        Manager = new StateManager<T>(Entity);
+    }
+
+    public StateManager<T> GetManager()
+    {
+        return Manager;
+    }
+
+    public void Run()
+    {
+        AbstractState<T> previous = currentState;
+        if (Manager.GetNewState(ref currentState))
+        {
+            previous.OnExit(Entity);
+            currentState.OnEnter(Entity);
+            currentState.InState(Entity);
+        }
+        else
+        {
+            currentState.InState(Entity);
+        }
+    }
+}
