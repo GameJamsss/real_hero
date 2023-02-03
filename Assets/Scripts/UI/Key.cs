14d1767eb3c1665e526eea56b7a10d3350069c49
MsArgona <elenapichugina2012@gmail.com>
2023-02-03 12:43:02 +0400

    Added more animations

--- Assets/Scripts/UI/Key.cs
+++ Assets/Scripts/UI/Key.cs
@@ -6,13 +6,20 @@ using TMPro;
 public class Key : MonoBehaviour
 {
     [SerializeField] private PlayerNameInput _playerInput;
-    [SerializeField] private string _symbol = "?";
+    private string _symbol;
     private TextMeshProUGUI _text;
 
     private void Awake()
     {
         _text = GetComponentInChildren<TextMeshProUGUI>();
-        _text.text = _symbol;
+        //_text.text = _symbol;
+        _symbol = _text.text;
+    }
+
+    private void Start()
+    {
+        if (_playerInput == null)
+            _playerInput = FindObjectOfType<PlayerNameInput>();
     }
 
     public void SentKey() => _playerInput.SentKeySymbol(_symbol);
