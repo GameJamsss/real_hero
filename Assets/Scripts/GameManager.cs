4ab20f0d0345633e37b1822eb27795633ea44b72
TIGNIT <timigntrt@gmail.com>
2023-07-31 23:35:54 +0700

    update timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,48 +6,49 @@ using Assets.Scripts.Entities.PlayerEntity;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour{
-    public Player player;
-    public Boss boss;
-    public float time;
+public class GameManager : MonoBehaviour
+{
+	public Player player;
+	public Boss boss;
+	public float time;
 
-    public GameMenuManager uiManager;
+	public GameMenuManager uiManager;
 
-    private void Start(){
-        Time.timeScale = 1;
+	private void Start()
+	{
+		Time.timeScale = 1;
 
-        Sub();
-    }
+		Sub();
+	}
 
-    public void Sub(){
-        player.damaged += uiManager.playerHealthBar.NextImage;
-        uiManager.playerHealthBar.death += uiManager.ShowDefeatPanel;
-        boss.damaged += uiManager.bossHealthBar.ChangeValue;
-        uiManager.bossHealthBar.death += uiManager.ShowWinPanel;
+	public void Sub()
+	{
+		player.damaged += uiManager.playerHealthBar.NextImage;
+		uiManager.playerHealthBar.death += uiManager.ShowDefeatPanel;
+		boss.damaged += uiManager.bossHealthBar.ChangeValue;
+		uiManager.bossHealthBar.death += uiManager.ShowWinPanel;
 
-    }
+	}
 
-    public void Restart(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        Time.timeScale = 1;
-    }
+	public void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		Time.timeScale = 1;
+	}
 
-    private void Update(){
-        if (time >= 0){
-            time = time - Time.deltaTime;
-            Debug.Log("TIME "+FormatTime(time));
-            uiManager._timer.text = FormatTime(time);
-            uiManager._resultTimer.text = FormatTime(time);
-        }
-        else{
-            uiManager.ShowDefeatPanel();
-        }
-    }
-    
-    public static string FormatTime(float timeInSeconds)
-    {
-        TimeSpan timeSpan = TimeSpan.FromSeconds(timeInSeconds);
-        string formattedTime = string.Format("{0:00}:{1:00}", timeSpan.Minutes, timeSpan.Seconds);
-        return formattedTime;
-    }
+	private void Update()
+	{
+		time = time + Time.deltaTime;
+		Debug.Log("TIME " + FormatTime(time));
+		uiManager._timer.text = FormatTime(time);
+		uiManager._resultTimer.text = FormatTime(time);
+		// uiManager.ShowDefeatPanel();
+	}
+
+	public static string FormatTime(float timeInSeconds)
+	{
+		TimeSpan timeSpan = TimeSpan.FromSeconds(timeInSeconds);
+		string formattedTime = string.Format("{0:00}:{1:00}", timeSpan.Minutes, timeSpan.Seconds);
+		return formattedTime;
+	}
 }
