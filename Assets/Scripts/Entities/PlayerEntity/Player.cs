3c35ee89e8cf0d3dd416dc598f1f8236fdf6cf81
TIGNIT <timigntrt@gmail.com>
2023-08-02 01:54:36 +0700

    update UI

--- Assets/Scripts/Entities/PlayerEntity/Player.cs
+++ Assets/Scripts/Entities/PlayerEntity/Player.cs
@@ -5,166 +5,168 @@ using UnityEngine;
 
 namespace Assets.Scripts.Entities.PlayerEntity
 {
-    public class Player : MonoBehaviour
-    {
-        [Header("Velocity")]
-        [SerializeField] public float Velocity = 5.0f;
+	public class Player : MonoBehaviour
+	{
+		[Header("Velocity")]
+		[SerializeField] public float Velocity = 5.0f;
 
-        [Header("Acceleration")]
-        [SerializeField] public float Acceleration = 2.0f;
+		[Header("Acceleration")]
+		[SerializeField] public float Acceleration = 2.0f;
 
-        [Header("Decceleration")]
-        [SerializeField] public float Decceleration = 2.0f;
+		[Header("Decceleration")]
+		[SerializeField] public float Decceleration = 2.0f;
 
-        [Header("Jump height")]
-        [SerializeField] public float JumpHeight = 5.0f;
+		[Header("Jump height")]
+		[SerializeField] public float JumpHeight = 5.0f;
 
-        [Header("Max air jumps")]
-        [SerializeField] public int MaxAirJumps = 1;
+		[Header("Max air jumps")]
+		[SerializeField] public int MaxAirJumps = 1;
 
-        [Header("Fall gravity scale")]
-        [SerializeField] public float FallGravityScale = 2f;
+		[Header("Fall gravity scale")]
+		[SerializeField] public float FallGravityScale = 2f;
 
-        [Header("The time need to be passed before new dash in milliseconds")]
-        [SerializeField] public float DashOffsetSec = 0.5f;
+		[Header("The time need to be passed before new dash in milliseconds")]
+		[SerializeField] public float DashOffsetSec = 0.5f;
 
-        [Header("How long player will be dashing without control in milliseconds")]
-        [SerializeField] public float UncontrollableDashTimeSec = 0.2f;
+		[Header("How long player will be dashing without control in milliseconds")]
+		[SerializeField] public float UncontrollableDashTimeSec = 0.2f;
 
-        [Header("How long player will be dashing in milliseconds overall")]
-        [SerializeField] public float DashTimeMilliAll = 0.4f;
+		[Header("How long player will be dashing in milliseconds overall")]
+		[SerializeField] public float DashTimeMilliAll = 0.4f;
 
-        [Header("Dash power")]
-        [SerializeField] public float DashPower = 8f;
+		[Header("Dash power")]
+		[SerializeField] public float DashPower = 8f;
 
-        [Header("Stun duration in seconds")]
-        [SerializeField] public float StunDuration = 2f;
+		[Header("Stun duration in seconds")]
+		[SerializeField] public float StunDuration = 2f;
 
-        [Header("Knock back power")]
-        [SerializeField] public float KnockBackPower = 2f;
+		[Header("Knock back power")]
+		[SerializeField] public float KnockBackPower = 2f;
 
-        [Header("Knock up power")]
-        [SerializeField] public float KnockUpPower = 2f;
+		[Header("Knock up power")]
+		[SerializeField] public float KnockUpPower = 2f;
 
-        [Header("Ground Layer Mask")]
-        [SerializeField] public LayerMask GroundMask;
+		[Header("Ground Layer Mask")]
+		[SerializeField] public LayerMask GroundMask;
 
-        [HideInInspector] public Collider2D Collider;
+		[HideInInspector] public Collider2D Collider;
 
-        [HideInInspector] public Animator Animation;
+		[HideInInspector] public Animator Animation;
 
-        [HideInInspector] public Rigidbody2D Rigidbody;
+		[HideInInspector] public Rigidbody2D Rigidbody;
 
-        [HideInInspector] public GameObject AttackObjectTest;
+		[HideInInspector] public GameObject AttackObjectTest;
 
-        [HideInInspector] public int AirJumpsCounter = 0;
+		[HideInInspector] public int AirJumpsCounter = 0;
 
-        [HideInInspector] public float CurrentDashTimeSec = 0f;
+		[HideInInspector] public float CurrentDashTimeSec = 0f;
 
-        [HideInInspector] public float CurrentDashOffsetSec = 0f;
+		[HideInInspector] public float CurrentDashOffsetSec = 0f;
 
-        [HideInInspector] public float LastDashVector = 0f;
+		[HideInInspector] public float LastDashVector = 0f;
 
-        [HideInInspector] public float CurrentStunDuration = 0f;
+		[HideInInspector] public float CurrentStunDuration = 0f;
 
-        StateMachine<Player> fsm;
-        
-        public GameObject AttackBall;
-        public SpriteRenderer blink;
+		StateMachine<Player> fsm;
 
-        public event Action<int> damaged ;
-        
-        public float attackCooldown = 1.0f; // Кулдаун между атаками
-        public float attackDuration = 1.0f; // Длительность атаки
+		public GameObject AttackBall;
+		public SpriteRenderer blink;
 
-        private bool isAttacking = false;
-        private float cooldownTimer = 0.0f;
-        
-        void Start()
-        {
-            
-            Animator animation = gameObject.GetComponent<Animator>();
-            if (animation) Animation = animation; else Debug.LogError(gameObject.name + " need Animation");
+		public event Action<int> damaged;
 
-            Collider2D col = gameObject.GetComponent<Collider2D>();
-            if (col) Collider = col; else Debug.LogError(gameObject.name + " need Collider2d");
+		public float attackCooldown = 1.0f; // Кулдаун между атаками
+		public float attackDuration = 1.0f; // Длительность атаки
 
-            Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
-            if (rb) Rigidbody = rb; else Debug.LogError(gameObject.name + " need Rigidbody2d");
+		private bool isAttacking = false;
+		private float cooldownTimer = 0.0f;
 
-            //GameObject attackArea = GameObject.Find("AttackArea");
-            //if (attackArea) AttackObjectTest = attackArea; else Debug.LogError("Need attack area object on: " + gameObject.name);
+		void Start()
+		{
 
-            LastDashVector = transform.localScale.x;
+			Animator animation = gameObject.GetComponent<Animator>();
+			if (animation) Animation = animation; else Debug.LogError(gameObject.name + " need Animation");
 
-            fsm = new StateMachine<Player>(
-                new StateManager<Player>(this)
-                .AddState(StateMap.Idle)
-                .AddState(StateMap.Move)
-                .AddState(StateMap.Jump)
-                .AddState(StateMap.Fall)
-                .AddState(StateMap.MoveUp)
-                .AddState(StateMap.Damage)
-                .AddState(StateMap.Dash)
-            );
-        }
+			Collider2D col = gameObject.GetComponent<Collider2D>();
+			if (col) Collider = col; else Debug.LogError(gameObject.name + " need Collider2d");
 
-        void Update()
-        {
-            fsm.Run();
-            //Debug.Log(fsm.CurrentState.Name);
-            
-            if (Input.GetButton("Fire1"))
-            {
-                Attack();
-            }
-            if (isAttacking)
-            {
-                cooldownTimer -= Time.deltaTime;
-                if (cooldownTimer <= 0)
-                {
-                    isAttacking = false;
-                }
-            }
-        }
+			Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+			if (rb) Rigidbody = rb; else Debug.LogError(gameObject.name + " need Rigidbody2d");
 
-        public void Damaged(int damage){
-                damaged?.Invoke(damage);
-                StartCoroutine(Blink());
-        }
+			//GameObject attackArea = GameObject.Find("AttackArea");
+			//if (attackArea) AttackObjectTest = attackArea; else Debug.LogError("Need attack area object on: " + gameObject.name);
 
-        public void Attack()
-        {
-            if (!isAttacking)
-            {
-                isAttacking = true;
-                cooldownTimer = attackCooldown;
-                AttackBall.SetActive(true);
-                Animation.Play("attack");
-                StartCoroutine(EndAtack());
-            }
-        }
-        private IEnumerator EndAtack()
-        {
-            // Ждем заданную длительность атаки
-            yield return new WaitForSeconds(attackDuration);
-            Animation.Play("Idle");
-            // Уничтожаем объект атаки
-            AttackBall.SetActive(false);
-        }
-        
-        private IEnumerator Blink()
-        {
-            blink.color=Color.red;
-            // Ждем заданную длительность атаки
-            yield return new WaitForSeconds(0.1f);
-            // Уничтожаем объект атаки
+			LastDashVector = transform.localScale.x;
 
-            blink.color = Color.white;
-        }
+			fsm = new StateMachine<Player>(
+				new StateManager<Player>(this)
+				.AddState(StateMap.Idle)
+				.AddState(StateMap.Move)
+				.AddState(StateMap.Jump)
+				.AddState(StateMap.Fall)
+				.AddState(StateMap.MoveUp)
+				.AddState(StateMap.Damage)
+				.AddState(StateMap.Dash)
+			);
+		}
 
-        public void Death(){
-            blink.color = Color.black;
-        }
-    }
+		void Update()
+		{
+			fsm.Run();
+			//Debug.Log(fsm.CurrentState.Name);
+
+			if (Input.GetButton("Fire1"))
+			{
+				Attack();
+			}
+			if (isAttacking)
+			{
+				cooldownTimer -= Time.deltaTime;
+				if (cooldownTimer <= 0)
+				{
+					isAttacking = false;
+				}
+			}
+		}
+
+		public void Damaged(int damage)
+		{
+			damaged?.Invoke(damage);
+			StartCoroutine(Blink());
+		}
+
+		public void Attack()
+		{
+			if (!isAttacking)
+			{
+				isAttacking = true;
+				cooldownTimer = attackCooldown;
+				AttackBall.SetActive(true);
+				Animation.Play("attack");
+				StartCoroutine(EndAtack());
+			}
+		}
+		private IEnumerator EndAtack()
+		{
+			// Ждем заданную длительность атаки
+			yield return new WaitForSeconds(attackDuration);
+			Animation.Play("Idle");
+			// Уничтожаем объект атаки
+			AttackBall.SetActive(false);
+		}
+
+		private IEnumerator Blink()
+		{
+			blink.color = Color.red;
+			// Ждем заданную длительность атаки
+			yield return new WaitForSeconds(0.1f);
+			// Уничтожаем объект атаки
+
+			blink.color = Color.white;
+		}
+
+		public void Death()
+		{
+			blink.color = Color.black;
+		}
+	}
 }
