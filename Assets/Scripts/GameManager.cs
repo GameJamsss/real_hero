dae29c3be8afef28d51099d7c47cdbc5efa5f363
TIGNIT <timigntrt@gmail.com>
2023-08-11 17:33:09 +0700

    update Pause Panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,67 +6,83 @@ using Assets.Scripts.Entities.PlayerEntity;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour{
-    public Player player;
-    public Boss boss;
-    public float time=0;
-    public bool pause;
-    public GameObject pauseText;
+public class GameManager : MonoBehaviour
+{
+	public Player player;
+	public Boss boss;
+	public float time = 0;
+	public bool pause;
+	public GameObject PausePanel;
 
-    public GameMenuManager uiManager;
+	public GameMenuManager uiManager;
 
-    private void Start(){
-        Time.timeScale = 1;
+	private void Start()
+	{
+		Time.timeScale = 1;
 
-        Sub();
-    }
+		Sub();
+	}
 
-    public void Sub(){
-        player.damaged += uiManager.playerHealthBar.NextImage;
-        uiManager.playerHealthBar.death += player.Death;
-        uiManager.playerHealthBar.death += uiManager.ShowDefeatPanel;
+	public void Sub()
+	{
+		player.damaged += uiManager.playerHealthBar.NextImage;
+		uiManager.playerHealthBar.death += player.Death;
+		uiManager.playerHealthBar.death += uiManager.ShowDefeatPanel;
 
-        boss.damaged += uiManager.bossHealthBar.ChangeValue;
-        uiManager.bossHealthBar.death += boss.Death;
-        uiManager.bossHealthBar.death += uiManager.ShowWinPanel;
+		boss.damaged += uiManager.bossHealthBar.ChangeValue;
+		uiManager.bossHealthBar.death += boss.Death;
+		uiManager.bossHealthBar.death += uiManager.ShowWinPanel;
 
-    }
+	}
 
-    public void Restart(){
-        Time.timeScale = 1;
-        SceneManager.LoadScene(0);
-    }
+	public void Restart()
+	{
+		Time.timeScale = 1;
+		SceneManager.LoadScene(0);
+	}
 
-    public void AfterWin(){
-        Time.timeScale = 1;
-        StaticData.isOpenEnterNameScreen = true;
-        StaticData.isCurrentTime=FormatTime(time);
-        SceneManager.LoadScene(0);
-    }
+	public void AfterWin()
+	{
+		Time.timeScale = 1;
+		StaticData.isOpenEnterNameScreen = true;
+		StaticData.isCurrentTime = FormatTime(time);
+		SceneManager.LoadScene(0);
+	}
 
-    
-    private void Update(){
-            time = time + Time.deltaTime;
-            uiManager._timer.text = FormatTime(time);
-            uiManager._resultTimer.text = FormatTime(time);
-            if (Input.GetKeyDown(KeyCode.P)){
-                pause = !pause;
-                if (pause){
-                    Time.timeScale = 0;
-                    pauseText.SetActive(true);
-                }
-                else{
-                    Time.timeScale = 1;
-                    pauseText.SetActive(false);
-                }
-            }
+	public void Continue()
+	{
+		pause = false;
+		Debug.Log("CONTIUE CLICKED");
+		Time.timeScale = 1;
+		PausePanel.SetActive(false);
+	}
 
-    }
-    
-    public static string FormatTime(float timeInSeconds)
-    {
-        TimeSpan timeSpan = TimeSpan.FromSeconds(timeInSeconds);
-        string formattedTime = string.Format("{0:00}:{1:00}", timeSpan.Minutes, timeSpan.Seconds);
-        return formattedTime;
-    }
+
+	private void Update()
+	{
+		time = time + Time.deltaTime;
+		uiManager._timer.text = FormatTime(time);
+		uiManager._resultTimer.text = FormatTime(time);
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			pause = !pause;
+			if (pause)
+			{
+				Time.timeScale = 0;
+				PausePanel.SetActive(true);
+			}
+			else
+			{
+				this.Continue();
+			}
+		}
+
+	}
+
+	public static string FormatTime(float timeInSeconds)
+	{
+		TimeSpan timeSpan = TimeSpan.FromSeconds(timeInSeconds);
+		string formattedTime = string.Format("{0:00}:{1:00}", timeSpan.Minutes, timeSpan.Seconds);
+		return formattedTime;
+	}
 }
